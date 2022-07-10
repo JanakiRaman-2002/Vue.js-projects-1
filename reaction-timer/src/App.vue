a026a6fbd40988cd1ad16cf70bd6b3e76e04eb34
connect2jans <janakiraman.k2020@vitstudent.ac.in>
2022-07-10 16:50:36 +0530

    Reflex-game

---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" class="buttonclass" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <!-- <p v-if="showResults">Reaction Time: {{ score }} ms</p> -->
+  <Results v-if="showResults" :score="score"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+export default {
+  name: 'App',
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+      console.log(this.delay)
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgb(197, 178, 178);
+  margin-top: 60px;
+}
+body {
+  background: black;
+}
+button {
+  width: 100px;
+  background: rgb(94, 221, 100);
+  height: 50px;
+  padding: 2px auto;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.buttonclass {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 500;
+  font-size: 1rem;
+}
+</style>
---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,43 @@
+<template>
+  <h3 class="scoreclass">Your reaction time was {{ score }} milliseconds</h3>
+  <h2 class="rankclass">{{ rank }}</h2>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score<300) {
+                this.rank = "Dayumm....Awesome reflexes";
+            }
+        else if (this.score>300 && this.score<500) {
+                this.rank = "That's the reflex I'm talkin' about.... Aim for Awesome Reflex";
+            }
+        else {
+                this.rank = "You're slow asf dude :(";
+            }
+        console.log("missed this oops");
+    },
+    methods: {}
+}
+</script>
+
+<style>
+    .rankclass {
+        color: green;
+    }
+    .scoreclass {
+        position:relative;
+        margin-left: 650px;
+        padding: 20px auto;
+        border-radius: 10px;
+        border: 2px solid green;
+        width: 600px;
+        height: 25px;
+    }
+</style>
